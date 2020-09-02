18a17637ab7f76c203df265868c65758386e9961
Nicholas Merolle <nmerolle11@hotmail.com>
2020-09-02 23:30:58 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,20 @@
 # Write your code here.
+
+
+def badge_maker(speaker)
+      return "Hello, my name is #{speaker}."
+end
+
+def batch_badge_creator(speakers)
+  name_arr = []
+  speakers.each do |name|
+    name_arr << "Hello, my name is #{name}."
+  end
+  return name_arr
+end
+
+def assign_rooms(speaker)
+  speaker.each.with_index(1) do |name, index|
+    puts "Hello, #{name}! You'll be assigned to room #{index}!"
+  end
+end
